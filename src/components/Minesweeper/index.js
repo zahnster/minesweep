b830b30c2979a9c11d0227755d6166b289b6f174
Jade Pennig <jade@pennig.name>
2017-01-09 10:31:15 -0600

    Core gameplay concepts are or less complete. Still some polish to do, lots of designs and future ideas.

--- src/components/Minesweeper/index.js
+++ src/components/Minesweeper/index.js
@@ -1,42 +1,61 @@
 import React, { Component } from 'react';
 import './style.css';
 
+const HAPPY_GAME = '🙂';
+const SAD_GAME = '😵';
+const COMPLETE_GAME = '😁';
+const BOMB = '💣';
+const DEFAULT_GAME_STATE = {
+    rows: 7,
+    cols: 7,
+    bombs: 5,
+    flags: 0,
+    bombsLeft: 5,
+    board: [], // { row: 0, col: 0, value: '💣', isSecret: true, isFlagged: false }
+    gameStatus: HAPPY_GAME  // 🙂 | 😵    
+};
+
 class Minesweeper extends Component {
     constructor() {
         super();
         
-        this.state = {
-            rows: 10,
-            cols: 10,
-            bombs: 20,
-            board: [] // { row: 1, col: 1, value: 'bomb', isSecret: true }
-        };
+        this.state = DEFAULT_GAME_STATE;
+        this.state.board = this.buildBoard();
 
-        this.buildBoard = this.buildBoard.bind(this);
-    }
-
-    componentWillMount() {
-        this.buildBoard();
+        this.reset = this.reset.bind(this);
     }
 
     componentDidMount() {
         document.querySelector('.minesweeper').addEventListener('click', (event) => {
-            console.log(event.target)
+            if (event.target.classList.contains('minesweeper-cell') && this.state.gameStatus === HAPPY_GAME) { 
+                this.selectCell(event.target);
+            }
+        });
+
+        document.querySelector('.minesweeper').addEventListener('contextmenu', (event) => {
+            if (this.state.gameStatus === HAPPY_GAME) {
+                this.flagCell(event.target);
+            }
+            event.preventDefault();                
         });
     }
 
+    /**
+     * Methods for building the board and placing the numbers
+     */
     buildBoard() {
         let board = [];
 
-        for(let row = 1; row <= this.state.rows; row++) {
+        for(let row = 0; row < this.state.rows; row++) {
             let rowArr = [];
 
-            for(let col = 1; col <= this.state.cols; col++) {
+            for(let col = 0; col < this.state.cols; col++) {
                 rowArr.push({
                     row,
                     col,
                     value: null, // value is set later
-                    isSecret: true
+                    isSecret: true,
+                    isFlagged: false
                 });
             }
 
@@ -46,7 +65,7 @@ class Minesweeper extends Component {
         board = this.addMinesToBoard(board);
         board = this.addNumbersToBoard(board);
 
-        this.setState({...this.state, board});
+        return board;
     }
 
     addMinesToBoard(board) {
@@ -58,7 +77,7 @@ class Minesweeper extends Component {
             const randCell = board[randRow][randCol];
 
             if (!randCell.value) {
-                board[randRow][randCol].value = 'bomb';
+                board[randRow][randCol].value = BOMB;
                 bombsPlaced++;
             }
         }
@@ -71,7 +90,7 @@ class Minesweeper extends Component {
             row.forEach((cell, cellIndex) => {
                 let bombCount = 0;
 
-                if (cell.value !== 'bomb') {
+                if (cell.value !== BOMB) {
                     // if not bomb, find all neighbors
                     [-1, 0, 1].forEach(offsetRow => {
                         [-1, 0, 1].forEach(offsetCol => {
@@ -82,7 +101,7 @@ class Minesweeper extends Component {
                             if (board[neighborRow] && board[neighborRow][neighborCol] && !(offsetRow === 0 && offsetCol === 0)) {
                                 const neighborCell = board[neighborRow][neighborCol];
 
-                                if (neighborCell.value === 'bomb') {
+                                if (neighborCell.value === BOMB) {
                                     bombCount++;
                                 }
                             }
@@ -101,35 +120,148 @@ class Minesweeper extends Component {
         return Math.round(Math.random() * (max - 1));
     }
 
+    /**
+     * Methods for Gameplay
+     */
+    selectCell(cellDOM) {
+        const row = cellDOM.getAttribute('data-row');
+        const col = cellDOM.getAttribute('data-cell');
+        let newState = {...this.state};
+        let cell = newState.board[row][col];
+
+        cell.isSecret = false;
+
+        switch(cell.value) {
+            case BOMB:
+                newState.gameStatus = SAD_GAME;
+            break;
+
+            case 0:
+                newState.board = this.selectNeighbors(cell, newState.board);
+            break;
+
+            default:
+            break;
+        }
+
+        this.setState(newState);
+    }
+
+    selectNeighbors(cell, board) {
+        const currentRow = cell.row;
+        const currentCol = cell.col;
+
+        cell.isSecret = false;
+
+        [-1, 0, 1].forEach(row => {
+            [-1, 0, 1].forEach(col => {
+                const newRow = currentRow + row;
+                const newCol = currentCol + col;
+
+                if (board[newRow] && board[newRow][newCol]) {
+                    const newCell = board[newRow][newCol];
+
+                    // recurse if needed
+                    if (newCell.value === 0 && newCell.isSecret) {
+                        board = this.selectNeighbors(newCell, board);
+                    }
+                    else {
+                        newCell.isSecret = false;
+                    }
+                }
+            });
+        });
+
+        return board;
+    }
+
+    flagCell(cellDOM) {
+        const row = cellDOM.getAttribute('data-row');
+        const col = cellDOM.getAttribute('data-cell');
+        let newState = {...this.state};
+        let cell = newState.board[row][col];
+
+        // toggle on
+        if (!cell.isFlagged) {
+            cell.isFlagged = true;
+            newState.flags++;
+
+            if (cell.value === BOMB) {
+                newState.bombsLeft--;
+            }
+        }
+        // toggle off
+        else {
+            cell.isFlagged = false;
+            newState.flags--;
+
+            if (cell.value === BOMB) {
+                newState.bombsLeft++;
+            }
+        }
+
+        if (newState.bombsLeft === 0) {
+            newState.gameStatus = COMPLETE_GAME;
+        }
+        
+        this.setState(newState);
+    }
+
+    /**
+     * Reset Game
+     */
+    reset() {
+        const newGame = DEFAULT_GAME_STATE;
+        newGame.board = this.buildBoard();
+
+        this.setState(newGame);
+    }
+
     render() {
         const board = this.state.board;
+        const status = this.state.gameStatus;
+        const bombsLeft = this.state.bombs - this.state.flags;
+        const startOverButton = (status === SAD_GAME || status === COMPLETE_GAME) ? <button className='start-over' onClick={this.reset}>Start Over?</button> : null;
 
         return (
             <div className='minesweeper'>
-                {board.map((row, rowIndex) => {
-                    return (
-                        <div className='minesweeper-row' key={rowIndex}>
-                            {row.map((cell, cellIndex) => {
-                                let cellValue = cell.isSecret ? null : cell.value;
-                                let cellClass = 'minesweeper-cell cell-unselected';
+                <div className='stats'>
+                    <dl>
+                        <dt>Bombs Left</dt>
+                        <dd>{bombsLeft}</dd>
 
-                                // testing only
-                                if (cell.value === 'bomb') {
-                                    cellClass = 'minesweeper-cell cell-bomb';
-                                }
-                                else {
-                                    cellValue = cell.value;
-                                }
+                        <dt>Status</dt>
+                        <dd>{status}</dd>
 
-                                return (
-                                    <div className={cellClass} key={cellIndex} data-row={rowIndex} data-cell={cellIndex}>
-                                        {cellValue}
-                                    </div>
-                                );
-                            })}
-                        </div>
-                    );
-                })}
+                        <dt>Actual Bombs Left</dt>
+                        <dd>{this.state.bombsLeft}</dd>
+                    </dl>
+                </div>
+
+                <div className='game-board'>
+                    {board.map((row, rowIndex) => {
+                        return (
+                            <div className='minesweeper-row' key={rowIndex}>
+                                {row.map((cell, cellIndex) => {
+                                    let cellValue = cell.isSecret ? null : cell.value;
+                                    let cellClass = 'minesweeper-cell';
+                                    cellClass += cell.isSecret ? ' cell-unselected' : ' cell-selected';
+
+                                    if (cellValue === 0) cellValue = '';
+                                    if (cell.isFlagged) cellValue = '🚩';
+
+                                    return (
+                                        <div className={cellClass} key={cellIndex} data-row={rowIndex} data-cell={cellIndex}>
+                                            {cellValue}
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        );
+                    })}
+                </div>
+
+                {startOverButton}
             </div>
         );
     }
